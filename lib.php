16688dfd8e520083b16f60f3b7117a7737a5a720
euisub <dmltjq60@gmail.com>
2021-01-14 14:15:28 +0000

    adasd

--- lib.php
+++ lib.php
@@ -1,13 +1,9 @@
 <?php
-
   $client_id = "YZt59p0BkWpwhZwMnwR_";
   $client_secret = "QFaGjwpsLQ";
-  $encText = urlencode($_POST["SEARVALUE"]);
- 
-  $START= $_POST["START"];
-
-  $url = "https://openapi.naver.com/v1/search/blog.json?query=".$encText."&display=100&start=".$START; // json 결과
-
+  $encText = urlencode("네이버오픈API");
+  $url = "https://openapi.naver.com/v1/search/blog.xml?query=".$encText; // json 결과
+//  $url = "https://openapi.naver.com/v1/search/blog.xml?query=".$encText; // xml 결과
   $is_post = false;
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, $url);
@@ -20,7 +16,8 @@
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
   $response = curl_exec ($ch);
   $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-  echo "status_code:".$status_code."";
+  echo "status_code:".$status_code."
+";
   curl_close ($ch);
   if($status_code == 200) {
     echo $response;
